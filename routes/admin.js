e93ce59884a48bd20141dfcb7b879a7f3d925d4b
Sourabh <dragneelfps@gmail.com>
2018-01-08 21:32:33 -0800

    Added new routes and updated old ones

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,17 @@
 var Admin = require('../models/admin');
+var uuid = require('uuid/v4');
 
 module.exports = function (app) {
 
     // Create admin account
     app.post('/api/admin', function (req, res) {
        var entry = new Admin(req.body);
+       entry.id_code = uuid();
        entry.save(function(err){
             if(err){
-                res.json({info: 'Error while creating admin account', error: err});
+                res.status(400).json({info: 'Error while creating admin account', error: err});
             }else{
-                res.json({info: 'Successfully created admin account'})
+                res.status(201).json({info: 'Successfully created admin account'})
             }
        });
     });
@@ -20,15 +22,33 @@ module.exports = function (app) {
             username: req.body.username,
             password: req.body.password
         },function (err, admin) {
-            if(err){ res.json({info: "Error during trying to log in", error: err}) }
+            if(err){ res.status(400).json({info: "Error during trying to log in", error: err}) }
             if(admin){
-                console.log(admin);
-                res.json({info: "Credentials correct", admin: admin});
+                res.status(200).json({info: "Credentials correct", admin: admin});
             }else{
-                res.json({info: "Incorrect credentials"});
+                res.status(404).json({info: "Incorrect credentials"});
             }
         });
     });
+
+    // Get admin id
+    app.get('/api/admin/id', function (req, res) {
+        if(req.query.id_code === undefined || req.query.id_code === '' ){
+            res.status(400).json({error: 'No id_code specified'})
+        }
+        console.log(req.query.id_code);
+        Admin.findOne({id_code: req.query.id_code}, function (err, admin) {
+            if(err){
+                res.status(400).json({error: err});
+            }
+            if(admin){
+                res.status(200).json({admin_id: admin._id});
+            }else{
+                res.status(404).json({info: 'not found'});
+            }
+        });
+    });
+
 
     // Update id_code
     app.post('/api/admin/idcode', function (req, res) {
@@ -42,14 +62,16 @@ module.exports = function (app) {
         {
             new: true
         }, function (err, admin) {
-        if(err){ res.json({info: "Error while trying to update id_code", error: err});}
+        if(err){ res.status(400).json({info: "Error while trying to update id_code", error: err});}
         if(admin){
-            res.json({info: "Update successful", admin: admin});
+            res.status(200).json({info: "Update successful", admin: admin});
         }else{
-            res.json({info: "Incorrect credentials"});
+            res.status(404).json({info: "Incorrect credentials"});
         }
         });
     });
+
+
 
 
 };
--- routes/client.js
+++ routes/client.js
@@ -14,6 +14,21 @@ module.exports = function (app) {
         });
     });
 
+    // Get all clients for admin_id param
+    app.get('/api/client', function (req, res) {
+        if(req.query.admin_id === undefined || req.query.admin_id === '' ){
+            res.status(400).json({error: 'No id_code specified'});
+        }
+        Client.find({admin: req.query.admin_id}).exec(function (err, clients) {
+            if(err){ res.status(400).json({error: err});}
+            if(clients){
+                res.status(200).json({clients: clients})
+            }else{
+                res.status(404).json({info: 'Not found'});
+            }
+        })
+    });
+
     // Login or Check credentials
     app.post('/api/client/login', function (req, res) {
         Client.findOne({
@@ -29,6 +44,8 @@ module.exports = function (app) {
             }
         });
     });
+
+
 
     // Update id_code
     app.post('/api/client/idcode', function (req, res) {
