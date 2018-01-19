21c328e0905b2676655a639bfbd28d7ee20899be
Sourabh <dragneelfps@gmail.com>
2018-01-19 11:28:49 +0530

    Misc

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync('config.json','utf8'));
 
-var express = require('express')
+var express = require('express');
 var app = express();
 
 var mongoose = require('mongoose');
-mongoose.connect(config.db_url);
+mongoose.connect(config.db_url, {
+    useMongoClient: true
+});
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -15,8 +17,10 @@ app.use(bodyParser.urlencoded({ extended: true}));
 var admin = require('./routes/admin')(app);
 var client = require('./routes/client')(app);
 
+app.get('/',function (req,res) {
+    res.send("Welcome");
+});
 
-
-var server = app.listen(config.server_port, function () {
+var server = app.listen(8000, function () {
     console.log('Server started on localhost:'+config.server_port);
 });
